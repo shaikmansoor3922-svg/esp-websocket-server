ba0aefad46337351f874b4f1e37759682b0eab5f
shaikmansoor3922-svg <shaikmansoor3922@gmail.com>
2026-02-13 23:46:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,42 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List
-from fastapi.responses import HTMLResponse
-import os
+import time
 
 app = FastAPI()
 
 latest_data = []
+data_history = []
+last_received_time = 0
 
 class SensorData(BaseModel):
     values: List[float]
 
+@app.get("/")
+def home():
+    return {"status": "Server Running"}
 
 @app.post("/upload")
 def upload_data(data: SensorData):
-    global latest_data
+    global latest_data, data_history, last_received_time
     latest_data = data.values
+    data_history.append(data.values)
+    last_received_time = time.time()   # store current time
     print("Received:", latest_data)
     return {"message": "Data received successfully"}
-
 
 @app.get("/latest")
 def get_latest():
     return {"values": latest_data}
 
+@app.get("/history")
+def get_history():
+    return {"history": data_history}
 
-@app.get("/", response_class=HTMLResponse)
-def dashboard():
-    with open("dashboard.html", "r") as f:
-        return f.read()
+@app.get("/status")
+def get_status():
+    # If no data for more than 5 seconds → disconnected
+    if time.time() - last_received_time > 5:
+        return {"device": "disconnected"}
+    else:
+        return {"device": "connected"}
