55ea8f9b5dd9c373516b53817cee3764179a11c6
shaikmansoor3922-svg <shaikmansoor3922@gmail.com>
2026-02-14 17:48:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,30 @@ import time
 from fastapi import Query
 from fastapi.responses import StreamingResponse
 from io import StringIO
+import sqlite3
+from datetime import datetime
+
+DB_NAME = "sensor_data.db"
+
+def init_db():
+    conn = sqlite3.connect(DB_NAME)
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS sensor_data (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            timestamp TEXT,
+            s1 REAL, s2 REAL, s3 REAL, s4 REAL,
+            s5 REAL, s6 REAL, s7 REAL, s8 REAL,
+            s9 REAL, s10 REAL, s11 REAL, s12 REAL,
+            s13 REAL, s14 REAL, s15 REAL, s16 REAL
+        )
+    """)
+
+    conn.commit()
+    conn.close()
+
+init_db()
 
 
 app = FastAPI()
